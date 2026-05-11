90565b86d65e39059716c7fe8b6f5c78bb07f013
Aleks-Zielinski <alekszielinski01@gmail.com>
2026-05-11 11:51:38 +0200

    update dokumentacji do weryfikacji czy git

--- Struktury/Funkcje/graham_scan.cpp
+++ Struktury/Funkcje/graham_scan.cpp
@@ -2,23 +2,7 @@
 
 // https://en.wikipedia.org/wiki/Graham_scan
 std::vector<Point> graham_scan(std::vector<Point> points){
-
-    /*
-    PRZYKŁADOWE PUNKTY Z WYKŁADU
-    std::vector<Point> points;
-    points.push_back(Point(0, 0));
-    points.push_back(Point(1, 0));
-    points.push_back(Point(2, 0));
-    points.push_back(Point(2, 1));
-    points.push_back(Point(4, 2));
-    points.push_back(Point(3, 4));
-    points.push_back(Point(1, 3));
-    points.push_back(Point(5, 1));
-    points.push_back(Point(3, 0));
-    points.push_back(Point(2, 2));
-    */
-
-    /// KROK 1: USUNIĘCIE DUPLIKATÓW I USTAWIENIE NAJBLIŻSZEGO
+    /// KROK 1: USUNIECIE DUPLIKATOW I USTAWIENIE NAJBLIZSZEGO
     ///         PUNKTU JAKO PIERWSZY (NAJPIERW NA Y, W RAZIE REMISU NA X)
 
     std::sort(points.begin(), points.end(), [](const Point& a, const Point& b) {
@@ -31,15 +15,15 @@ std::vector<Point> graham_scan(std::vector<Point> points){
         return cmpDouble(a.x, b.x) == 0 && cmpDouble(a.y, b.y) == 0;
     }), points.end());
 
-    /// PRZYPADKI, GDY JEST PONIŻEJ 3 PUNKTÓW
+    /// PRZYPADKI, GDY JEST PONIZEJ 3 PUNKTOW
     if (points.size() < 3) {
         if (points.size() == 0) return {};
         if(points.size() == 1 || points.size() == 2) return points;
     }
 
-    /// KROK 2: SORTUJEMY I DOSTOSOWUJEMY LISTĘ PUNKTÓW ZE WZGLĘDU NA WSPÓŁRZĘDNE KĄTOWE
+    /// KROK 2: SORTUJEMY I DOSTOSOWUJEMY LISTE PUNKTOW ZE WZGLEDU NA WSPOLRZEDNE KATOWE
 
-    /// NAJPIERW SORTUJEMY LISTĘ PUNKTÓW
+    /// NAJPIERW SORTUJEMY LISTE PUNKTOW
     std::sort(points.begin() + 1, points.end(), [&](const Point& pi, const Point& pj){
         double d = det(points[0], pi, pj);
         if (sgn(d) == 0) {
@@ -52,7 +36,7 @@ std::vector<Point> graham_scan(std::vector<Point> points){
         return sgn(d) > 0;
     });
 
-    /// USUWAMY Z LISTY BLIŻSZE PUNKTY Z TĄ SAMĄ WSPÓŁRZĘDNĄ KĄTOWĄ CO DALSZY PUNKT
+    /// USUWAMY Z LISTY BLIZSZE PUNKTY Z TA SAMA WSPOLRZEDNA KATOWA CO DALSZY PUNKT
     std::vector<Point> furtherPoints;
     furtherPoints.push_back(points[0]);
 
@@ -64,7 +48,7 @@ std::vector<Point> graham_scan(std::vector<Point> points){
     
     points = furtherPoints;
 
-    /// KROK 3: UTWORZENIE STOSU I DODANIE 3 PIERWSZYCH PUNKTÓW
+    /// KROK 3: UTWORZENIE STOSU I DODANIE 3 PIERWSZYCH PUNKTOW
 
     if (points.size() < 3) return points;
 
@@ -73,7 +57,7 @@ std::vector<Point> graham_scan(std::vector<Point> points){
     stack.push_back(points[1]);
     stack.push_back(points[2]);
 
-    /// KROK 4: OSTATECZNE DOBRANIE PUNKTÓW OTOCZKI
+    /// KROK 4: OSTATECZNE DOBRANIE PUNKTOW OTOCZKI
 
     for (unsigned int i = 3; i < points.size(); i++) {
         while (stack.size() >= 2 && sgn(det(stack[stack.size() - 2], stack.back(), points[i])) <= 0) {
